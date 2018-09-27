f77d25c37e8b320e5f7a1ca4db9aa34085dae4c9
Andrey Petrov <andrew.petrov@emc.com>
2018-09-27 14:22:27 +0300

    uncomment maim.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
-/*
+#include "supportive_functions.hpp"
+
+using funct=std::function<double(double)>
+
+void solveEquation(funct )
+{
+
+}
+
 int main()
 {
   std::cout << "Hello, World!" << std::endl;
   return 0;
 }
- */
+
